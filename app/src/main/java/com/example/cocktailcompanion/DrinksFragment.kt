bfc8d757002f44c40c8fd41a0efd20e92babf31d
Boots <cnd9351@rit.edu>
2019-04-30 19:04:18 -0400

    list view made data not populating

---
+++ app/src/main/java/com/example/cocktailcompanion/DrinksFragment.kt
@@ -0,0 +1,69 @@
+package com.example.cocktailcompanion
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.cocktailcompanion.dummy.DummyContent
+import com.example.cocktailcompanion.dummy.DummyContent.DummyItem
+
+/**
+ * A fragment representing a list of Items.
+ * Activities containing this fragment MUST implement the
+ * [DrinksFragment.OnListFragmentInteractionListener] interface.
+ */
+class DrinksFragment : Fragment() {
+
+    companion object {
+        val drinks = "drinks"
+    }
+    // TODO: Customize parameters
+    private var columnCount = 1
+
+    private var listener: OnListFragmentInteractionListener? = null
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_drinks_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyDrinksRecyclerViewAdapter(DummyContent.ITEMS, listener)
+            }
+        }
+        return view
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson
+     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onListFragmentInteraction(item: DummyItem?)
+    }
+}
--- app/src/main/java/com/example/cocktailcompanion/SearchDrinksbyName.kt
+++ app/src/main/java/com/example/cocktailcompanion/SearchDrinksbyName.kt
@@ -1,27 +1,12 @@
 package com.example.cocktailcompanion
-
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.gson.GsonBuilder
-import kotlinx.android.synthetic.main.fragment_search_drinksby_name.*
-import okhttp3.Call
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import java.io.IOException
+
 
 class SearchDrinksbyName : Fragment() {
-    private val client = OkHttpClient()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
